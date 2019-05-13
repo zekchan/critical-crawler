93659c126df9580049da57371e862711c5b92b46
Ilya Groshev <groshev.i.d@gmail.com>
2019-05-13 15:45:50 +0300

    fix ua bug

--- crawlPage.js
+++ crawlPage.js
@@ -6,111 +6,107 @@ const fs = require('fs').promises
 const prom = require('prom-client');
 
 const crawlingTime = new prom.Histogram({
-  name: 'cc_crawling_time',
-  help: 'Crawling time',
-  buckets: prom.linearBuckets(5, 1, 10)
+    name: 'cc_crawling_time',
+    help: 'Crawling time',
+    buckets: prom.linearBuckets(5, 1, 10)
 });
 
 const DEBUG = process.env.DEBUG === 'true'
 const CHROME_BIN = process.env.CHROME_BIN
 
 const MOBILE_SIZES = {
-  width: 400,
-  height: 800
+    width: 400,
+    height: 800
 }
 
 const DESKTOP_SIZES = {
-  width: 1440,
-  height: 900
+    width: 1440,
+    height: 900
 }
 
 if (!CHROME_BIN) {
-  throw new Error('CHROME_BIN env variable is mandatory!')
+    throw new Error('CHROME_BIN env variable is mandatory!')
 }
 
 async function waitForJSIdle(page) {
-  return  page.evaluate(() => {
-    return new Promise(resolve => {
-      requestIdleCallback(resolve)
-    })
-  });
+    return page.evaluate(() => {
+        return new Promise(resolve => {
+            requestIdleCallback(resolve)
+        })
+    });
 }
 async function crawlPage(url, headers, key, isMobile) {
-  const end = crawlingTime.startTimer()
-  const browser = await puppeteer.launch({
-    headless: !DEBUG,
-    ignoreHTTPSErrors: true,
-    args: ['--disable-setuid-sandbox', '--no-sandbox'],
-    executablePath: process.env.CHROME_BIN
-  })
-  console.log('Browser launched')
-  const page = await browser.newPage()
-  console.log('Tab created')
-  const allCss = []
-  page.on('response', async response => {
-    if (response.request().resourceType() === 'stylesheet') {
-      allCss.push(await response.text())
-      console.log('New CSS')
-      console.log(response.url())
+    const end = crawlingTime.startTimer()
+    const browser = await puppeteer.launch({
+        headless: !DEBUG,
+        ignoreHTTPSErrors: true,
+        args: ['--disable-setuid-sandbox', '--no-sandbox'],
+        executablePath: process.env.CHROME_BIN
+    })
+    console.log('Browser launched')
+    const page = await browser.newPage()
+    console.log('Tab created')
+    const allCss = []
+    page.on('response', async response => {
+        if (response.request().resourceType() === 'stylesheet') {
+            allCss.push(await response.text())
+            console.log('New CSS')
+            console.log(response.url())
+        }
+    });
+    const UA = headers['User-Agent'] || headers['User-agent'] || headers['user-agent']
+    await page.setExtraHTTPHeaders(headers)
+    if (UA) {
+        await page.setUserAgent(UA)
     }
-  });
-  await page.setExtraHTTPHeaders(headers)
-  if (headers['user-agent']) {
-    await page.setUserAgent(headers['user-agent'])
-  }
-  if (isMobile) {
-    await page.setViewport({
-      width: 1024,
-      height: 1000,
-      isMobile: true,
-      hasTouch: true
+    if (isMobile) {
+        await page.setViewport({
+            ...MOBILE_SIZES,
+            isMobile: true,
+            hasTouch: true
+        })
+    } else {
+        await page.setViewport(DESKTOP_SIZES)
+    }
+
+    console.log('Opening page')
+    await page.goto(url, {
+        waitUntil: [
+            'networkidle0',
+            'load',
+            'domcontentloaded'
+        ]
     })
-  } else {
-    await page.setViewport({
-      width: 1440,
-      height: 2000,
+    console.log('Page opened')
+    await waitForJSIdle(page)
+    console.log('Page idle')
+    const allCssString = allCss.join('\n')
+    await page.addStyleTag({ content: allCssString })
+    await page.waitFor(5000)
+    const html = await page.evaluate(() => document.documentElement.outerHTML);
+    console.log('Html got')
+    await page.close()
+    const fileName = path.resolve(__dirname, `${md5(key)}.html`)
+    await fs.writeFile(fileName, html)
+    const criticalCss = await penthouse({
+        url: `file:///${fileName}`,
+        keepLargerMediaQueries: true,
+        userAgent: UA,
+        customPageHeaders: headers,
+        cssString: allCssString,
+        puppeteer: {
+            getBrowser: () => browser
+        },
+        ...(isMobile ? MOBILE_SIZES : DESKTOP_SIZES)
     })
-  }
-  
-  console.log('Opening page')
-  await page.goto(url, {
-    waitUntil: [
-      'networkidle0',
-      'load',
-      'domcontentloaded'
-    ]
-  })
-  console.log('Page opened')
-  await waitForJSIdle(page)
-  console.log('Page idle')
-  const allCssString = allCss.join('\n')
-  await page.addStyleTag({ content: allCssString })
-  await page.waitFor(5000)
-  const html = await page.evaluate(() => document.documentElement.outerHTML);
-  console.log('Html got')
-  await page.close()
-  const fileName = path.resolve(__dirname, `${md5(key)}.html`)
-  await fs.writeFile(fileName, html)
-  const criticalCss = await penthouse({
-    url: `file:///${fileName}`,
-    keepLargerMediaQueries: true,
-    userAgent: headers['user-agent'],
-    customPageHeaders: headers,
-    cssString: allCssString,
-    renderWaitTime: 4000,
-    puppeteer: {
-      getBrowser: () => browser
-    },
-    ...(isMobile ? MOBILE_SIZES : DESKTOP_SIZES)
-  })
-  
-  console.log('Critical css got')
-  await browser.close()
-  
-  console.log('Browser closed')
-  await fs.unlink(fileName)
-  end()
-  return criticalCss;
+
+    console.log('Critical css got')
+    await browser.close()
+
+    console.log('Browser closed')
+    await fs.unlink(fileName)
+    end()
+    return criticalCss;
 }
 
-module.exports = crawlPage
+module.exports = crawlPagew
--- index.js
+++ index.js
@@ -12,35 +12,35 @@ const crawlPage = require('./crawlPage')
 
 prom.collectDefaultMetrics({ prefix: 'cc_' })
 const responseTime = new prom.Histogram({
-  name: 'cc_response_time',
-  help: 'Response time',
-  buckets: prom.linearBuckets(0, 0.005, 10)
+    name: 'cc_response_time',
+    help: 'Response time',
+    buckets: prom.linearBuckets(0, 0.005, 10)
 });
 const cacheHit = new prom.Counter({
-  name: 'cc_cache_hit',
-  help: 'cache_hit',
+    name: 'cc_cache_hit',
+    help: 'cache_hit',
 });
 const cacheMiss = new prom.Counter({
-  name: 'cc_cache_miss',
-  help: 'cache_miss',
+    name: 'cc_cache_miss',
+    help: 'cache_miss',
 });
 const errorsCount = new prom.Counter({
-  name: 'cc_errors',
-  help: 'all request errors',
+    name: 'cc_errors',
+    help: 'all request errors',
 });
 const crawlingErrorsCount = new prom.Counter({
-  name: 'cc_crawling_errors',
-  help: 'errors in crawling script',
+    name: 'cc_crawling_errors',
+    help: 'errors in crawling script',
 });
 const queueSize = new prom.Gauge({
-  name: 'cc_queue_size',
-  help: 'Amount ok keys that should be processed',
+    name: 'cc_queue_size',
+    help: 'Amount ok keys that should be processed',
 });
 const t = new Limiter({ concurrency: 1 });
 const R = {
-  get: promisify(client.get).bind(client),
-  set: promisify(client.set).bind(client),
-  del: promisify(client.del).bind(client)
+    get: promisify(client.get).bind(client),
+    set: promisify(client.set).bind(client),
+    del: promisify(client.del).bind(client)
 }
 
 const app = new Koa()
@@ -48,51 +48,51 @@ PROCESSING = 'PROCESSING'
 app.use(route.get('/healthz', ctx => ctx.body = 'OK'))
 app.use(route.get('/metrics', ctx => ctx.body = prom.register.metrics()))
 app.use(timeout(20, { status: 499 }))
-app.use(async (ctx, next) => {
-  const end = responseTime.startTimer()
-  try {
-    await next()
-  } catch (e) {
-    console.error(e)
-    errorsCount.inc()
-  }
-  end()
+app.use(async(ctx, next) => {
+    const end = responseTime.startTimer()
+    try {
+        await next()
+    } catch (e) {
+        console.error(e)
+        errorsCount.inc()
+    }
+    end()
 })
 app.use(bodyParser())
-app.use(route.post('/', async (ctx) => {
-  const { headers, version, url, key: optionalKey } = ctx.request.body
-  const deviceInfo = isMobileJs(headers['user-agent'])
-  const deviceName = (deviceInfo.phone && 'phone') || (deviceInfo.tablet && 'tablet') || 'desktop'
-  const key = `${optionalKey || url}-${version}-${deviceName}`
-  const mayBeCss = await R.get(key)
-  if (mayBeCss && (mayBeCss !== PROCESSING)) {
-    ctx.status = 200;
-    ctx.body = mayBeCss;
-    cacheHit.inc()
-    return
-  }
-  ctx.status = 404;
-  ctx.body = 'Preparing css';
-  cacheMiss.inc()
-  if (!mayBeCss) {
-    process.nextTick(async () => {
-      await R.set(key, PROCESSING)
-      queueSize.inc()
-      t.push(async (cb) => {
-        try {
-          await R.set(key, await crawlPage(url, headers, key, deviceInfo.mobile))
-        } catch (e) {
-          console.error(e)
-          R.del(key)
-          crawlingErrorsCount.inc()
-        } finally {
-          queueSize.dec()
-          cb()
-        }
-      })
-    })
-  }
+app.use(route.post('/', async(ctx) => {
+    const { headers, version, url, key: optionalKey } = ctx.request.body
+    const deviceInfo = isMobileJs(headers['User-Agent'] || headers['User-agent'] || headers['user-agent'])
+    const deviceName = (deviceInfo.phone && 'phone') || (deviceInfo.tablet && 'tablet') || 'desktop'
+    const key = `${optionalKey || url}-${version}-${deviceName}`
+    const mayBeCss = await R.get(key)
+    if (mayBeCss && (mayBeCss !== PROCESSING)) {
+        ctx.status = 200;
+        ctx.body = mayBeCss;
+        cacheHit.inc()
+        return
+    }
+    ctx.status = 404;
+    ctx.body = 'Preparing css';
+    cacheMiss.inc()
+    if (!mayBeCss) {
+        process.nextTick(async() => {
+            await R.set(key, PROCESSING)
+            queueSize.inc()
+            t.push(async(cb) => {
+                try {
+                    await R.set(key, await crawlPage(url, headers, key, deviceInfo.mobile))
+                } catch (e) {
+                    console.error(e)
+                    R.del(key)
+                    crawlingErrorsCount.inc()
+                } finally {
+                    queueSize.dec()
+                    cb()
+                }
+            })
+        })
+    }
 }))
 app.listen(3000, () => {
-  console.log('started')
+    console.log('started')
 })
